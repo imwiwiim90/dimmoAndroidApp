11059f41f31bb406d3499c1dad5b8c35188c6edb
WILSOND <wilsondaniel95@hotmail>
2016-05-22 03:14:29 -0500

    som

--- app/src/main/java/hckthn/dimmo/CompletePaymentActivity.java
+++ app/src/main/java/hckthn/dimmo/CompletePaymentActivity.java
@@ -2,6 +2,10 @@ package hckthn.dimmo;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import hckthn.dimmo.model.PaymentHelper;
 
 /**
  * Created by WILSOND on 5/22/16.
@@ -10,5 +14,10 @@ public class CompletePaymentActivity extends AppCompatActivity{
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.dimmo_complete_payment_activity);
+
+        TextView nameFood = (TextView) ((ViewGroup)findViewById(R.id.food)).findViewById(R.id.name);
+        nameFood.setText(PaymentHelper.food.name);
+
     }
 }
